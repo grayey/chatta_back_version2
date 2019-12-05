048c378ce92190b3daad43a05e3bebc70f0821ea
chrismeeky <nwodochristian@gmail.com>
2019-12-05 14:25:44 +0100

    updated local branch

--- src/client/client.controller.ts
+++ src/client/client.controller.ts
@@ -1,14 +1,15 @@
 import {
-    Controller,
-    Get,
-    Post,
-    Put,
-    Delete,
-    Patch,
-    Body,
-    Req,
-    Res,
-    Param, UseGuards,
+  Controller,
+  Get,
+  Post,
+  Put,
+  Delete,
+  Patch,
+  Body,
+  Req,
+  Res,
+  Param,
+  UseGuards,
 } from '@nestjs/common';
 import { CreateClientDto } from './dto/create-client.dto';
 import { ClientsService } from './client.service';
@@ -20,59 +21,52 @@ import { AuthGuard } from '@nestjs/passport';
 
 @Controller('client')
 export class ClientController {
-    constructor(private readonly clientService: ClientsService) {
-    }
+  constructor(private readonly clientService: ClientsService) {}
 
-    @Get()
-    findAll(): Promise<Client[]> {
-        return this.clientService.findAll();
-    }
+  @Get()
+  findAll(): Promise<Client[]> {
+    return this.clientService.findAll();
+  }
 
-    @Get(':id')
-    findOne(@Param('id') id): Promise<Client> {
-        return this.clientService.findOne(id);
-    }
+  @Get(':id')
+  findOne(@Param('id') id): Promise<Client> {
+    return this.clientService.findOne(id);
+  }
 
-    @Post()
-    create(@Body() createClientDto): Promise<Client> {
-        return this.clientService.create(createClientDto);
-    }
+  //   @Post()
+  //   create(@Body() createClientDto): Promise<Client> {
+  //     return this.clientService.create(createClientDto);
+  //   }
 
-    @Delete(':id')
-    delete(@Param('id') id): Promise<Client> {
-        return this.clientService.delete(id);
-    }
+  @Delete(':id')
+  delete(@Param('id') id): Promise<Client> {
+    return this.clientService.delete(id);
+  }
 
-    @Put(':id')
-    update(
-        @Body() updateClientDto,
-        @Param('id') id,
-    ): Promise<Client> {
-        return this.clientService.update(id, updateClientDto);
-    }
+  @Put(':id')
+  update(@Body() updateClientDto, @Param('id') id): Promise<Client> {
+    return this.clientService.update(id, updateClientDto);
+  }
 
-    @Post()
-    async signUp(
-        @Body() createClientDto,
-        @Req() res: Response,
-        @Res() req: Request,
-    ): Promise<Client> {
-        return this.clientService.signUp(createClientDto, res, req);
-    }
+  @Post()
+  async signUp(
+    @Body() createClientDto,
+    @Req() res: Response,
+    @Res() req: Request,
+  ): Promise<Client> {
+    return this.clientService.signUp(createClientDto, res, req);
+  }
 
-    @Get('test')
-    @UseGuards(AuthGuard('jwt'))
-    testAuthRoute() {
-        return {
-            message: 'you did it',
-        };
-    }
+  @Get('test')
+  @UseGuards(AuthGuard('jwt'))
+  testAuthRoute() {
+    return {
+      message: 'you did it',
+    };
+  }
 
-
-    @Patch(':id')
-    findByIdAndToggleEnable(@Param('id') id): Promise<Client> {
-        return this.clientService.findByIdAndToggleEnable(id);
-    }
+  @Patch(':id')
+  findByIdAndToggleEnable(@Param('id') id): Promise<Client> {
+    return this.clientService.findByIdAndToggleEnable(id);
+  }
 }
-
-
