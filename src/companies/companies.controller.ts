47711b232c695a31c2e82c2ce36a434f2a71ff15
voke1 <vokeolomu@yahoo.com>
2020-01-16 20:31:36 +0100

    fix companies controller

--- src/companies/companies.controller.ts
+++ src/companies/companies.controller.ts
@@ -1,4 +1,51 @@
-import { Controller } from '@nestjs/common';
+import {
+    Controller,
+    Get,
+    Post,
+    Put,
+    Delete,
+    Patch,
+    Body,
+    Req,
+    Res,
+    Param,
+    UseGuards,
+} from '@nestjs/common';
+import { CompaniesService } from './companies.service';
+import { CompaniesInterface} from './companies.interface';
 
 @Controller('companies')
-export class CompaniesController {}
+export class CompaniesController {
+
+    constructor(private readonly clientService: CompaniesService) { }
+
+    @Get()
+    findAll(): Promise<CompaniesInterface[]> {
+        return this.clientService.findAll();
+    }
+
+    @Get(':id')
+    findOne(@Param('id') id): Promise<CompaniesInterface> {
+        return this.clientService.findOne(id);
+    }
+
+    @Post()
+    create(@Body() createClientDto): Promise<CompaniesInterface> {
+        return this.clientService.create(createClientDto);
+    }
+
+    @Delete(':id')
+    delete(@Param('id') id): Promise<CompaniesInterface> {
+        return this.clientService.delete(id);
+    }
+
+    @Put(':id')
+    update(@Body() updateClientDto, @Param('id') id): Promise<CompaniesInterface> {
+        return this.clientService.update(id, updateClientDto);
+    }
+
+    // @Patch(':id')
+    // findByIdAndToggleEnable(@Param('id') id): Promise<CompaniesInterface> {
+    //     return this.clientService.findByIdAndToggleEnable(id);
+    // }
+}
