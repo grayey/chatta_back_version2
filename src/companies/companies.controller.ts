5a110306cc7d427491283837223fc76dfb3d416a
voke1 <vokeolomu@yahoo.com>
2020-01-28 11:10:00 +0100

    update codebase

--- src/companies/companies.controller.ts
+++ src/companies/companies.controller.ts
@@ -17,31 +17,31 @@ import { CompaniesInterface} from './companies.interface';
 @Controller('companies')
 export class CompaniesController {
 
-    constructor(private readonly clientService: CompaniesService) { }
+    constructor(private readonly companyService: CompaniesService) { }
 
     @Get()
     findAll(): Promise<CompaniesInterface[]> {
-        return this.clientService.findAll();
+        return this.companyService.findAll();
     }
 
     @Get(':id')
     findOne(@Param('id') id): Promise<CompaniesInterface> {
-        return this.clientService.findOne(id);
+        return this.companyService.findOne(id);
     }
 
     @Post('new')
     create(@Body() createClientDto): Promise<CompaniesInterface> {
-        return this.clientService.create(createClientDto);
+        return this.companyService.create(createClientDto);
     }
 
     @Delete(':id')
     delete(@Param('id') id): Promise<CompaniesInterface> {
-        return this.clientService.delete(id);
+        return this.companyService.delete(id);
     }
 
     @Put(':id')
     update(@Body() updateClientDto, @Param('id') id): Promise<CompaniesInterface> {
-        return this.clientService.update(id, updateClientDto);
+        return this.companyService.update(id, updateClientDto);
     }
 
     // @Patch(':id')
