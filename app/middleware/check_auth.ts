532e67af5c5c5db767d1a2aa12ae55b45dc96b23
voke1 <vokeolomu@yahoo.com>
2020-05-11 22:29:06 +0100

    fix error

--- app/middleware/check_auth.ts
+++ app/middleware/check_auth.ts
@@ -1,14 +1,14 @@
-import jwt from 'jsonwebtoken'
+// import jwt from 'jsonwebtoken'
 
-module.exports = (req, res, next) => {
-    try {
-        const token = req.headers.authorization.split(" ")[1];
-        const decoded = jwt.verify(token, process.env.JWT_KEY);
-        req.userData = decoded;
-        next();
-    } catch (error) {
-        return res.status(401).json({
-            message: 'Auth failed'
-        });
-    }
-};
+// module.exports = (req, res, next) => {
+//     try {
+//         const token = req.headers.authorization.split(" ")[1];
+//         const decoded = jwt.verify(token, process.env.JWT_KEY);
+//         req.userData = decoded;
+//         next();
+//     } catch (error) {
+//         return res.status(401).json({
+//             message: 'Auth failed'
+//         });
+//     }
+// };
--- app/route/clients.ts
+++ app/route/clients.ts
@@ -1,5 +1,5 @@
-const express = require("express");
-const router = express.Router();
+// const express = require("express");
+// const router = express.Router();
 const multer = require('multer');
 const checkAuth = require('../middleware/check-auth');
 const ProductsController = require('../controllers/products');
@@ -30,14 +30,14 @@ const upload = multer({
   fileFilter: fileFilter
 });
 
-router.get("/", ProductsController.products_get_all);
+// router.get("/", ProductsController.products_get_all);
 
-router.post("/", checkAuth, upload.single('productImage'), ProductsController.products_create_product);
+// router.post("/", checkAuth, upload.single('productImage'), ProductsController.products_create_product);
 
-router.get("/:productId", ProductsController.products_get_product);
+// router.get("/:productId", ProductsController.products_get_product);
 
-router.patch("/:productId", checkAuth, ProductsController.products_update_product);
+// router.patch("/:productId", checkAuth, ProductsController.products_update_product);
 
-router.delete("/:productId", checkAuth, ProductsController.products_delete);
+// router.delete("/:productId", checkAuth, ProductsController.products_delete);
 
-module.exports = router;
+// module.exports = router;
