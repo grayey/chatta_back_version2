1b6ecf5808cf637a614d02908890c668cf8e08c3
Christian Nwodo <nwodochristian@gmail.com>
2020-05-20 13:00:54 -0400

    creates email route

--- src/offlineHandler/offline.controller.ts
+++ src/offlineHandler/offline.controller.ts
@@ -1,45 +1,57 @@
-import { Controller, Post, Body, Req, Res } from '@nestjs/common';
-import { OfflineService } from './offline.service';
-import { Offline } from './interfaces/offline.interface';
+import {
+  Controller,
+  Post,
+  Body,
+  Req,
+  Res,
+  UseGuards,
+  Param,
+} from "@nestjs/common";
+import { OfflineService } from "./offline.service";
+import { Offline } from "./interfaces/offline.interface";
 
-import { Response, Request } from 'express';
-import { AuthGuard } from '@nestjs/passport';
+import { Response, Request } from "express";
+import { AuthGuard } from "../middleware/auth.guard";
+
 // @Controller('client')
 
-@Controller('email')
+@Controller("api/v1/email")
 export class OfflineController {
   constructor(private readonly offlineService: OfflineService) {}
 
-  @Post('offline')
+  @Post("offline")
   async offline(
     @Body() createOfflineDto,
     @Req() res: Response,
-    @Res() req: Request,
+    @Res() req: Request
   ): Promise<Offline> {
     return this.offlineService.sendOfflineMessage(createOfflineDto, res, req);
   }
-  @Post('request-demo')
+  @Post("call-to-action/:id")
+  @UseGuards(new AuthGuard())
   async requestDemo(
+    @Param("id") id,
     @Body() createOfflineDto,
     @Req() res: Response,
-    @Res() req: Request,
+    @Res() req: Request
   ): Promise<Offline> {
-    return this.offlineService.sendRequestDemoMessage(
+    return this.offlineService.sendCallToActionEmail(
+      id,
       createOfflineDto,
       res,
-      req,
+      req
     );
   }
-  @Post('meeting-scheduled')
+  @Post("meeting-scheduled")
   async scheduleMeeting(
     @Body() createOfflineDto,
     @Req() res: Response,
-    @Res() req: Request,
+    @Res() req: Request
   ): Promise<Offline> {
     return this.offlineService.sendScheduleMeetingMessage(
       createOfflineDto,
       res,
-      req,
+      req
     );
   }
 }
